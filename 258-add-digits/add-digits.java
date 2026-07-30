ba40c60557359ba4dbc0fa5d12b879d87bed0d0a
Prashanth Goud <147410233+Prashanthgoud15@users.noreply.github.com>
2026-07-30 20:26:38 +0530

    Time: 1 ms (97.96%) | Memory: 42.6 MB (45.13%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=0,r,n;
+        if(num ==0){
+            return 0;
+        }else if(num%9==0){
+            return 9;
+        }else{
+            return num%9;
+        }
+        
+    }
+}
