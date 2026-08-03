73b10d3d7775096dccfa5ef27e9913d62ef3f231
Prashanth Goud <147410233+Prashanthgoud15@users.noreply.github.com>
2026-08-03 10:47:48 +0530

    Time: 105 ms (43.19%) | Memory: 51.3 MB (23.03%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        
+        boolean prime[] = new boolean[n+1];
+        Arrays.fill(prime,true);
+        prime[0] = false;
+        prime[1] = false;
+        for(int i=2;(long)i*i<n;i++){
+            if(prime[i]){
+                for(long j=(long)i*i;j<n;j+=i){
+                   prime[(int)j]=false;
+                }
+            }
+        }
+        int c=0;
+        for(int i=2;i<n;i++){
+            if(prime[i]){
+                c++;
+            }
+        }
+        return c;
+       
+    }
+}
